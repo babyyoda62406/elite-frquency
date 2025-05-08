49d47307f05b6401d4b3dfc55eef531e39bfaad3
Deivis Torres Mena <babyyoda62406@gmail.com>
2025-05-08 09:30:32 -0400

    fix try 1

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,24 +2,27 @@ import type React from "react"
 import { locales } from "@/middleware"
 import { getDictionary } from "@/dictionaries"
 
+import type { ReactNode } from "react"
+
+interface RootLayoutProps {
+  children: ReactNode
+  params: {
+    lang: string
+  }
+}
+
+
 export async function generateStaticParams() {
   return locales.map((lang) => ({ lang }))
 }
 
-export default async function RootLayout({
-  children,
-  params,
-}: {
-  children: React.ReactNode
-  params: { lang: string }
-}) {
+export default async function RootLayout({ children, params }: RootLayoutProps) {
   const dict = await getDictionary(params.lang)
 
   return (
     <html lang={params.lang}>
       <head>
         <meta charSet="UTF-8" />
-        {/* favico */}
         <link rel="icon" href="2025-05-08_06-40.png" />
       </head>
       <body>
