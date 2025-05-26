0188f499ef39f8308034d1d081beee339475bb5b
Deivis Torres Mena <babyyoda62406@gmail.com>
2025-05-25 21:07:10 -0400

    google analtics

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -13,7 +13,7 @@ export default async function RootLayout({
   children: React.ReactNode
   params: Promise<{ lang: string }>
 }) {
-  const {lang }= await params
+  const { lang } = await params
   const dict = await getDictionary(lang)
 
   return (
@@ -23,7 +23,20 @@ export default async function RootLayout({
         {/* favico */}
         <link rel="icon" href="2025-05-08_06-40.png" />
         <meta name="google-site-verification" content="OcbEL0psibJMEZkx7TilTM5Ep-Rgvj_V6AiUEfPuX10" />
-        
+
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-269RPEYBSY"></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-269RPEYBSY');
+            `,
+          }}
+        />
       </head>
       <body>
         <div className="flex min-h-screen flex-col">{children}</div>
